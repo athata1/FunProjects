e73c2d243926f5253be1074dc843c2983c92add9
Akhil Thata <akhil.thata@gmail.com>
2021-12-11 22:27:03 -0500

    Added exporting to CSV and other forms of filtering

--- GenEdCondenser/src/Main.java
+++ GenEdCondenser/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -20,7 +21,9 @@ public class Main
             while (decision == 0) {
                 System.out.println("What would you like to do");
                 System.out.println("1. Filter by course type abbreviation.");
-                System.out.println("2. Quit");
+                System.out.println("2. Filter by level of course");
+                System.out.println("3. Export to csv");
+                System.out.println("4. Quit");
                 try {
                     decision = Integer.parseInt(scan.nextLine());
                 } catch (Exception e) {
@@ -47,11 +50,83 @@ public class Main
                     System.out.println(s);
                 }
                 System.out.println("Removed " + count + " courses");
-                System.out.println(courses.size() + "remains.");
+                System.out.println(courses.size() + " remains.");
                 System.out.println();
             }
             else if (decision == 2)
+            {
+                System.out.println("Enter a the maximum level of course you want to take");
+                System.out.println("Note: the level will be in the form of a number in the 100s divisible by 100" +
+                        "ex: 100, 200, 300");
+                int level = 0;
+                try
+                {
+                    level = Integer.parseInt(scan.nextLine());
+                    level += 100;
+                } catch(NumberFormatException e)
+                {
+                    System.out.println("Error: Enter a number in the hundreds divisible by 100 ex: 100, 200, 300");
+                    continue;
+                }
+
+                int count = 0;
+                for (int i = courses.size() - 1; i >= 0; i--)
+                {
+                    int countUnitNum = 0;
+                    String[] data = courses.get(i).split("[\\ | \\t]+");
+                    while (countUnitNum < data.length)
+                    {
+                        try
+                        {
+                            if (Integer.parseInt(data[countUnitNum].trim()) > level * 100) {
+                                courses.remove(i);
+                                count++;
+                            }
+                            break;
+                        } catch(Exception e)
+                        {
+                            countUnitNum++;
+                            continue;
+                        }
+                    }
+                }
+                System.out.println();
+                for(String s: courses)
+                {
+                    System.out.println(s);
+                }
+                System.out.println("Removed " + count + " courses");
+                System.out.println(courses.size() + " remains.");
+                System.out.println();
+            }
+            else if (decision == 3)
+            {
+                System.out.println("Enter filename for .csv");
+                String input = scan.nextLine();
+                if (!input.substring(input.length() - 4).equals(".csv"))
+                {
+                    input += ".csv";
+                }
+                System.out.println(input);
+                File f = new File(input);
+                if (f.exists())
+                {
+                    System.out.println("Error: file already exists");
+                    continue;
+                }
+                f.createNewFile();
+                PrintWriter writer = new PrintWriter(input);
+                for (String s: courses)
+                {
+                    writer.write(s.replace("\\t"," ") + "\n");
+                }
+                writer.flush();
+                writer.close();
+            }
+            else if (decision == 4)
+            {
                 break;
+            }
             else
                 System.out.println("Error: select a number between 1 and 2 inclusive");
         }
